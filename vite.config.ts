e3c6d737bcf228d58cabc967a9f7e5c564f3adcc
rapexa <rapexamrs@gmail.com>
2025-10-11 07:53:52 -0700

    update cache control

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +16,7 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
